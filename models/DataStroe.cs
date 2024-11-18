17e19a1965b9f529a06e8dd0bd7cf4974996fe89
BxDxnxxl <danielcaton44@gmail.com>
2024-11-18 21:59:18 +0100

    se crean los inicializadores de datos y se corrigen varios errores en clases

---
+++ models/DataStroe.cs
@@ -0,0 +1,11 @@
+namespace models
+{
+    public static class DataStore
+    {
+        //listas para almacenar todos los datos que se creen, se usa como si fuese una especie de BBDD
+        public static List<Pelicula> Peliculas { get; set; } = new List<Pelicula>();
+        public static List<Sala> Salas { get; set; } = new List<Sala>();
+        public static List<Sesion> Sesiones { get; set; } = new List<Sesion>();
+        public static List<Asiento> Asientos { get; set; } = new List<Asiento>();
+    }
+}
