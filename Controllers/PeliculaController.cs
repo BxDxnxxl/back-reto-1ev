1ac24628d6255811b40fa987b5d141f57de3a8f1
BxDxnxxl <danielcaton44@gmail.com>
2024-11-18 22:20:58 +0100

    actualizacion conttrolador peliculas y program

--- Controllers/PeliculaController.cs
+++ Controllers/PeliculaController.cs
@@ -13,29 +13,11 @@ namespace back.Controllers
     public class PeliculaController : ControllerBase
     {
 
-    //creación de lista de peliculas en las que almacenaremos los datos que se manejan en la API sustituyendo la bbdd
-    private static List<Pelicula> peliculas = new List<Pelicula>();
-
-    //funcion de creacion de peliculas que se usan para añadir a la lista creada anteriormente
-    public static void InicializarDatos()
-    {
-        peliculas.Add(new Pelicula("Inception", "Un thriller de ciencia ficción", "Christopher Nolan", new[] { "Acción", "Aventura" }, new DateTime(2010, 7, 16),
-            "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg","140"));
-        
-        peliculas.Add(new Pelicula("The Matrix", "Una historia ciberpunk revolucionaria", "Lana Wachowski y Lilly Wachowski", new[] { "Acción", "Ciencia Ficción" }, new DateTime(1999, 3, 31),
-            "https://m.media-amazon.com/images/I/613ypTLZHsL.jpg", "136"));
-        
-        peliculas.Add(new Pelicula("Interstellar", "Exploración de los límites del espacio y el tiempo", "Christopher Nolan", new[] { "Aventura", "Ciencia Ficción" }, new DateTime(2014, 11, 7),
-            "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktZGVlOC00M2QzLTkxN2EtZWE0ZmRjZTg5ZDJhXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg", "169"));
-
-    }
-
-
     //metodo que devuelve todas las peliculas
     [HttpGet]
     public ActionResult<IEnumerable<Pelicula>> GetPeliculas()
     {
-        return Ok(peliculas);
+        return Ok(DataStore.Peliculas);
     }
 
 
@@ -43,7 +25,7 @@ namespace back.Controllers
     [HttpGet("{id}")]
     public ActionResult<Pelicula> GetPelicula(int id)
     {
-        var pelicula = peliculas.FirstOrDefault(p => p.Id == id);
+        var pelicula = DataStore.Peliculas.FirstOrDefault(p => p.Id == id);
         if (pelicula == null)
         {
             return NotFound();
@@ -55,7 +37,7 @@ namespace back.Controllers
     [HttpPost]
     public ActionResult<Pelicula> CreatePelicula(Pelicula pelicula)
     {
-        peliculas.Add(pelicula);
+        DataStore.Peliculas.Add(pelicula);
         return CreatedAtAction(nameof(GetPelicula), new { id = pelicula.Id }, pelicula);
     }
 
@@ -65,46 +47,49 @@ namespace back.Controllers
     public IActionResult UpdatePelicula(int id, Pelicula peliculaActualizada)
     {
         //busca el id de la pelicula y comprueba que existe
-        var pelicula = peliculas.FirstOrDefault(p => p.Id == id);
+        var pelicula = DataStore.Peliculas.FirstOrDefault(p => p.Id == id);
         if (pelicula == null)
         {
             return NotFound();
         }
         
-        if (peliculaActualizada.Nombre != "string")
-        {
-            pelicula.Nombre = peliculaActualizada.Nombre;
-        }
+         if (!string.IsNullOrWhiteSpace(peliculaActualizada.Nombre))
+            {
+                pelicula.Nombre = peliculaActualizada.Nombre;
+            }
+            if (!string.IsNullOrWhiteSpace(peliculaActualizada.Descripcion))
+            {
+                pelicula.Descripcion = peliculaActualizada.Descripcion;
+            }
+            if (!string.IsNullOrWhiteSpace(peliculaActualizada.Director))
+            {
+                pelicula.Director = peliculaActualizada.Director;
+            }
+            if (peliculaActualizada.Categorias != null && peliculaActualizada.Categorias.Any())
+            {
+                pelicula.Categorias = peliculaActualizada.Categorias;
+            }
+            if (peliculaActualizada.AnioDeSalida != default(DateTime))
+            {
+                pelicula.AnioDeSalida = peliculaActualizada.AnioDeSalida;
+            }
+            if (!string.IsNullOrWhiteSpace(peliculaActualizada.ImagenBannerUrl))
+            {
+                pelicula.ImagenBannerUrl = peliculaActualizada.ImagenBannerUrl;
+            }
+            if (!string.IsNullOrWhiteSpace(peliculaActualizada.ImagenPequeniaUrl))
+            {
+                pelicula.ImagenPequeniaUrl = peliculaActualizada.ImagenPequeniaUrl;
+            }
+            if (!string.IsNullOrWhiteSpace(peliculaActualizada.Duracion))
+            {
+                pelicula.Duracion = peliculaActualizada.Duracion;
+            }
+            if (!string.IsNullOrWhiteSpace(peliculaActualizada.Valoracion))
+            {
+                pelicula.Valoracion = peliculaActualizada.Valoracion;
+            }
 
-        if (peliculaActualizada.Descripcion != "string")
-        {
-            pelicula.Descripcion = peliculaActualizada.Descripcion;
-        }
-
-        if (peliculaActualizada.Director != "string")
-        {
-            pelicula.Director = peliculaActualizada.Director;
-        }
-
-        if (peliculaActualizada.Categorias != null)
-        {
-            pelicula.Categorias = peliculaActualizada.Categorias;
-        }
-
-        if (peliculaActualizada.AnioDeSalida != default(DateTime))
-        {
-            pelicula.AnioDeSalida = peliculaActualizada.AnioDeSalida;
-        }
-
-        if (peliculaActualizada.ImagenUrl  != "string")
-        {
-            pelicula.ImagenUrl = peliculaActualizada.ImagenUrl;
-        }
-
-        if (peliculaActualizada.Duracion != "string")
-        {
-            pelicula.Duracion = peliculaActualizada.Duracion;
-        }
             return NoContent();
         }
 
@@ -113,13 +98,13 @@ namespace back.Controllers
     public IActionResult DeletePelicula(int id)
     {
         //busca la pelicula por id y comprueba que exista
-        var pelicula = peliculas.FirstOrDefault(p => p.Id == id);
+        var pelicula = DataStore.Peliculas.FirstOrDefault(p => p.Id == id);
         if (pelicula == null)
         {
             return NotFound();
         }
         //si ha encontrado la pelicula, la borra de la lista
-        peliculas.Remove(pelicula);
+        DataStore.Peliculas.Remove(pelicula);
         return NoContent();
     }
 
@@ -131,7 +116,7 @@ namespace back.Controllers
         var peliculasFiltradas = new List<Pelicula>();
 
         // con el foreach se comprobará cada pelicula existente si contiene las categorias
-        foreach (var pelicula in peliculas)
+        foreach (var pelicula in DataStore.Peliculas)
         {
             if (pelicula.Categorias.Contains(categoria))
             {
