ba54c8ef17df55cbea740c6763b068f20751f76c
BxDxnxxl <danielcaton44@gmail.com>
2024-11-29 00:28:07 +0100

    ajustado modelo de compra

--- models/Compra.cs
+++ models/Compra.cs
@@ -7,18 +7,21 @@ public class Compra
     private static int contadorId = 1;
     public int Id { get; private set; }
     public string Usuario { get; set; }
+    public List<string> AsientosPosiciones {get; set;}  = new List<string>();
+    public int Sala {get; set;}
     public int NumEntradas {get; set;}
     public double Precio { get; set; }
     public DateTime Fecha{ get; set; }
 
-    public Compra(string usuario, int numentradas, double precio, DateTime fecha)
+    public Compra(string usuario, int numentradas, double precio, DateTime fecha, List<string> asientosPosiciones, int sala)
     {
         Id = contadorId++;
         Usuario = usuario;
         NumEntradas = numentradas;
         Precio = precio;
         Fecha = fecha;
-        Fecha = fecha;
+        AsientosPosiciones = asientosPosiciones;
+        Sala = sala;
 
     }
 
